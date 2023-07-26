f0a861d851ebfe2bd63440ff6f69eb6f31ef1b29
JACKS9969 <101617802+JACKS9969@users.noreply.github.com>
2023-07-26 20:41:07 +0530

    new

---
+++ src/header/Header.js
@@ -0,0 +1,30 @@
+import './Header.css';
+import { FaFacebookSquare, FaInstagramSquare, FaTwitterSquare } from "react-icons/fa";
+
+function Header() {
+    return(
+        <nav className="header">
+            <div className="logo">
+                <h1>Food Plaza</h1>
+            </div>
+            <div className="main">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+            <div className="icon">
+                <ul>
+                    <li><a href="#"><FaFacebookSquare /></a></li>
+                    <li><a href="#"><FaInstagramSquare /></a></li>
+                    <li><a href="#"><FaTwitterSquare /></a></li>
+                </ul>
+            </div>
+        </nav>
+        
+    )
+};
+
+export default Header;
---
+++ src/main/Main.js
@@ -0,0 +1,94 @@
+import './Main.css';
+
+
+
+
+function Main() {
+    return(
+        <div className='main'>
+            <div className='container'>
+                <div className='container-left'>
+                    <h1>DINNER</h1>
+                    <p>BEST PLACE FOR DINNER</p>
+                </div>
+                <div className='container-right'>
+                    <img src="./images/Food1.jpg" alt="" />
+                </div>
+            </div>
+            <div className='container'>
+                <div className='container-left'>
+                    <h1>DINNER</h1>
+                    <p>BEST PLACE FOR DINNER</p>
+                </div>
+                <div className='container-right'>
+                    <img src="" alt="" />
+                </div>
+            </div>
+            <div className='container'>
+                <div className='container-left'>
+                    <h1>DINNER</h1>
+                    <p>BEST PLACE FOR DINNER</p>
+                </div>
+                <div className='container-right'>
+                    <img src="" alt="" />
+                </div>
+            </div>
+            <div className='container'>
+                <div className='container-left'>
+                    <h1>DINNER</h1>
+                    <p>BEST PLACE FOR DINNER</p>
+                </div>
+                <div className='container-right'>
+                    <img src="" alt="" />
+                </div>
+            </div>
+            <div className='container'>
+                <div className='container-left'>
+                    <h1>DINNER</h1>
+                    <p>BEST PLACE FOR DINNER</p>
+                </div>
+                <div className='container-right'>
+                    <img src="" alt="" />
+                </div>
+            </div>
+            <div className='container'>
+                <div className='container-left'>
+                    <h1>DINNER</h1>
+                    <p>BEST PLACE FOR DINNER</p>
+                </div>
+                <div className='container-right'>
+                    <img src="" alt="" />
+                </div>
+            </div>
+            <div className='container'>
+                <div className='container-left'>
+                    <h1>DINNER</h1>
+                    <p>BEST PLACE FOR DINNER</p>
+                </div>
+                <div className='container-right'>
+                    <img src="" alt="" />
+                </div>
+            </div>
+            <div className='container'>
+                <div className='container-left'>
+                    <h1>DINNER</h1>
+                    <p>BEST PLACE FOR DINNER</p>
+                </div>
+                <div className='container-right'>
+                    <img src="" alt="" />
+                </div>
+            </div>
+            <div className='container'>
+                <div className='container-left'>
+                    <h1>DINNER</h1>
+                    <p>BEST PLACE FOR DINNER</p>
+                </div>
+                <div className='container-right'>
+                    <img src="" alt="" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Main;
